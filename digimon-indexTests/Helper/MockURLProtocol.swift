cbda1a97bee21ee1c09e1db89606d0beabc25f90
EzraArya <ezrawijaya10@gmail.com>
2026-04-12 22:02:04 +0700

    Feat: Add MockURLProtocol for NetworkService Test

---
+++ digimon-indexTests/Helper/MockURLProtocol.swift
@@ -0,0 +1,40 @@
+//
+//  MockURLProtocol.swift
+//  digimon-index
+//
+//  Created by Ezra Arya Wijaya on 12/04/26.
+//
+
+import Foundation
+import XCTest
+
+class MockURLProtocol: URLProtocol {
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+    
+    override func startLoading() {
+        guard let handler = Self.requestHandler else {
+            XCTFail("MockURLProtocol.requestHandler is not set")
+            return
+        }
+        
+        do {
+            let (response, data) = try handler(request)
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() {
+    }
+}
