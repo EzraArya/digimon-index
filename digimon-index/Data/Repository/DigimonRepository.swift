4ba67d3fcf1211418805a6177394729ec159e102
EzraArya <ezrawijaya10@gmail.com>
2026-04-12 22:17:47 +0700

    feat: Add DigimonRepository and DigimonRepositoryProtocol

---
+++ digimon-index/Data/Repository/DigimonRepository.swift
@@ -0,0 +1,24 @@
+//
+//  DigimonRepository.swift
+//  digimon-index
+//
+//  Created by Ezra Arya Wijaya on 12/04/26.
+//
+
+import Foundation
+
+final class DigimonRepository: DigimonRepositoryProtocol {
+    let networkService: NetworkServiceProtocol
+    
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+    
+    func getDigimonList(page: Int, pageSize: Int, name: String?) async throws -> DigimonListResponse {
+        try await networkService.request(endpoint: .digimonList(page: page, pageSize: pageSize, name: name))
+    }
+    
+    func getDigimonDetail(id: Int) async throws -> DigimonDetail {
+        try await networkService.request(endpoint: .digimonDetail(id: id))
+    }
+}
---
+++ digimon-index/Domain/Protocols/DigimonRepositoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  DigimonRepositoryProtocol.swift
+//  digimon-index
+//
+//  Created by Ezra Arya Wijaya on 12/04/26.
+//
+
+protocol DigimonRepositoryProtocol {
+    func getDigimonList(page: Int, pageSize: Int, name: String?) async throws -> DigimonListResponse
+    func getDigimonDetail(id: Int) async throws -> DigimonDetail
+}
+
+extension DigimonRepositoryProtocol {
+    func getDigimonList(page: Int, pageSize: Int) async throws -> DigimonListResponse {
+        try await getDigimonList(page: page, pageSize: pageSize, name: nil)
+    }
+}
