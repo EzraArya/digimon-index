48411dc0c27cd221719f0e97b3cdd1e0c34324c0
EzraArya <ezrawijaya10@gmail.com>
2026-04-13 09:31:39 +0700

    feat: Add attribute and level filter placeholder

--- digimon-index/Domain/Models/DigimonSearchFilter.swift
+++ digimon-index/Domain/Models/DigimonSearchFilter.swift
@@ -14,8 +14,22 @@ struct DigimonSearchFilter: Equatable {
 
 enum AttributeFilter: String, CaseIterable, Codable {
     case data = "Data"
+    case free = "Free"
+    case virus = "Virus"
+    case vaccine = "Vaccine"
+    case unknown = "Unknown"
+    case variable = "Variable"
+    case noData = "No Data"
 }
 
 enum LevelFilter: String, CaseIterable, Codable {
-    case data = "Data"
+    case babyII = "Baby II"
+    case adult = "Adult"
+    case perfect = "Perfect"
+    case child = "Child"
+    case babyI = "Baby I"
+    case ultimate = "Ultimate"
+    case armor = "Armor"
+    case unknown = "Unknown"
+    case hybrid = "Hybrid"
 }
